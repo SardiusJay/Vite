a63ce92de27f8cfe2d3a4ddb57aceef47289e433
SARDIUS <71199152+SardiusJay@users.noreply.github.com>
2022-11-24 10:57:58 +0100

    cooking with Vite and React

---
+++ src/components/welcome.jsx
@@ -0,0 +1,25 @@
+import Sammy from "../img/sammy.jpg";
+import "../CSS/main.css"
+
+
+export default function Welcome() {
+    return (
+      <>
+        <div className="wrapper">
+          <h1>Welcome King Sardius🤴🏼</h1>
+          <p>This is going to be the coolest app in the world!</p>
+          <img src={Sammy} alt="Sammy image" width={200} height={200} />
+        </div>
+        <div className="wrapper">
+        <h1>Welcome King Sardius🤴🏼</h1>
+        <p>This is going to be the coolest app in the world!</p>
+        <img src={Sammy} alt="Sammy image" width={200} height={200} />
+      </div>
+      <div className="wrapper">
+      <h1>Welcome King Sardius🤴🏼</h1>
+      <p>This is going to be the coolest app in the world!</p>
+      <img src={Sammy} alt="Sammy image" width={200} height={200} />
+    </div>
+      </>
+    );
+  }
